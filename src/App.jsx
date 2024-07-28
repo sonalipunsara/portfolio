a4f3b9c530fd87b7050cd9c76af6124c80eabb53
sonalipunsara <sonalipunsara@gmail.com>
2024-07-28 12:40:52 +0530

    about-me section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
+import About from './Components/About/About'
 
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <div>
       <Navbar/>
       <Hero/>
+      <About/>
     </div>
   )
 }
