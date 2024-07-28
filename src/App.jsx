88c8ca1c3905c4b69f5e8eeef75015c5ecbc536c
sonalipunsara <sonalipunsara@gmail.com>
2024-07-28 23:34:08 +0530

    services section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import About from './Components/About/About'
+import Services from './Components/Services/Services'
 
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
       <Navbar/>
       <Hero/>
       <About/>
+      <Services/>
     </div>
   )
 }
